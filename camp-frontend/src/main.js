1ce5b6d2d0e3161638c740b4fdc12cb72d3ef9ed
lrx <1012246892@qq.com>
2019-08-09 18:21:04 +0800

    引入饿了么框架，配置登陆页面路由

--- camp-frontend/src/main.js
+++ camp-frontend/src/main.js
@@ -4,6 +4,7 @@ import router from './router/router'
 import Axios from 'axios'
 import store from './store'
 import element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 Vue.prototype.$message = element.Message;
---
+++ camp-frontend/src/views/login/login.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-form>
+    <el-form-item>
+      <el-input suffix="el-icon-user" placaholder="请输入账号" v-model="userForm.username">
+        <template slot="prepend">
+          <i class="userIcon"></i>
+        </template>
+      </el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-input suffix="el-icon-user" placaholder="请输入密码">
+        <template slot="prepend">
+          <i class="pasIcon"></i>
+        </template>
+      </el-input>
+    </el-form-item>
+  </el-form>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      userForm: {
+        username: null,
+        password: null
+      }
+    };
+  },
+  methods: {}
+};
+</script>
+<style lang="scss" scoped>
+.userIcon {
+    
+}
+</style>
