e0c5a3adfdd8d2ae0383ca440237fb4636cfe4eb
lutayy <1161792926@qq.com>
2020-02-17 16:57:57 +0800

    增加redis做二级缓存

--- camp-backend/src/main/java/com/lutayy/campbackend/controller/TestController.java
+++ camp-backend/src/main/java/com/lutayy/campbackend/controller/TestController.java
@@ -2,23 +2,38 @@ package com.lutayy.campbackend.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.lutayy.campbackend.common.util.Md5;
+import com.lutayy.campbackend.common.util.RedisUtil;
+import com.lutayy.campbackend.dao.StudentMapper;
+import com.lutayy.campbackend.pojo.Student;
+import com.lutayy.campbackend.pojo.StudentExample;
 import com.lutayy.campbackend.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @Controller
 @RequestMapping("/campback")
 public class TestController {
 
     @Autowired
-    StudentService studentService;
+    StudentMapper studentMapper;
+    @Resource
+    private RedisUtil redisUtil;
 
     @RequestMapping("/test")
     @ResponseBody
-    public String getall(){
-        String a="1231";
-        return Md5.digest(a);
+    public void getall(){
+        String result="";
+        StudentExample studentExample=new StudentExample();
+        System.out.println((Student)redisUtil.get("studentI_1"));
+    }
+
+    public static void main(String[] args){
+        TestController testController=new TestController();
+        testController.getall();
     }
 }
